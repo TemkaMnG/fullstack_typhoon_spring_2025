ccc804d002729ecc9f1a60c543b5375fa79321c2
Temka Tami <102396972+TemkaMnG@users.noreply.github.com>
2025-04-24 19:04:08 +0800

    add: session april 24

---
+++ test.py
@@ -0,0 +1,15 @@
+# for i in range(10, 100):
+#     print(i)
+
+# i = 1
+# while i <= 100:
+#     print(i)
+#     i += 1
+
+# i = 20
+# while i <= 60:
+#     print(i)
+#     i += 2
+
+for i in range(10):
+    print("Hello World!")
