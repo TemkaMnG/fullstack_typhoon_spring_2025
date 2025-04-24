a8d929c59c67c17d6f34ec62b336a61d3a533423
Tamir <typhoon@admin-03s-iMac.local>
2025-04-24 14:00:28 +0800

    add: commit mont

---
+++ test.py
@@ -0,0 +1,13 @@
+def product(x, y):
+    return x * y
+
+def difference(x, y):
+    return x - y
+
+x = 2
+y = 5
+value1 = product(x, y)
+value2 = difference(y, x)
+
+result = difference(value1, value2)
+print(result)
